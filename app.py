9800c5021189c212734d3c507ddc668b8b6a10fd
atdiausx2 <117471838+atdiausx2@users.noreply.github.com>
2022-11-26 11:38:17 +0300

    commit with sentence correction check implemented

--- app.py
+++ app.py
@@ -6,6 +6,31 @@ from dash import dcc
 # import dash_html_components as html
 from dash import html
 
+
+def loadData():
+    allCorrectedData = pd.read_csv("./essays_20211214.csv", sep=",")
+
+
+
+def getLevel():
+
+def selectSubmission(allCorrectedData, n_sentences):
+    if n_sentences == 1:
+
+        asignment_suggestion = np.random.choice(a=np.random.choice(
+            allCorrectedData[allCorrectedData['mother_tongues'] == mother_tongue_code][
+                'original_text'].to_list()).split("."),
+                                                size=n_sentences,
+                                                replace=False
+
+                                                )
+    else:
+        suggestionData = allCorrectedData[allCorrectedData['mother_tongues'] == mother_tongue_code].reset_index().iloc[
+            sentence_idx]
+        suggested_answer = "".join(suggestionData['original_text'].split(".")[:n_sentences])
+        correct_answer = "".join(suggestionData['corrected_text'].split(".")[:n_sentences])
+
+
 def constructLayout( app):
     app.layout = html.Div([])
     pass
@@ -21,8 +46,10 @@ constructLayout(app)
 
 app.title = Monolinguo
 
+
+
 # fixme: turn this off if not intending to deploy
-server = app.server
+# server = app.server
 # self.server = app.server
 # app = Monolinguo(username="kadara")
 # app.run()
